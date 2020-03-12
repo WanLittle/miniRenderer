c65e747557dbd0b4a1fb97f0dc72b5a0ed792ec4
WanLittle <2621996061@qq.com>
2020-03-12 21:22:56 +0800

    feat: 窗口填充颜色

--- miniRenderer/main.cpp
+++ miniRenderer/main.cpp
@@ -1,27 +1,111 @@
-#include "SDL.h" 
+#include <iostream>
+#include "SDL.h"
+
+Uint32 getpixel(SDL_Surface* surface, int x, int y); // 获取颜色
+void putpixel(SDL_Surface* surface, int x, int y, Uint32 pixel); // 填充颜色
 
 int main(int argc, char* argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
+    // 初始化 SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
 
-    SDL_Window* window = SDL_CreateWindow(
-        "SDL2Test",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        640,
-        480,
-        0
-    );
+    // 初始化窗口
+    SDL_Window* win = SDL_CreateWindow("mini renderer", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (win == nullptr) {
+        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    SDL_Surface* screen = SDL_GetWindowSurface(win);
 
-    SDL_Delay(3000);
+    for (int i = 0; i < 640; ++i)
+    {
+        for (int j = 0; j < 480; j++)
+        {
+            int x = i;
+            int y = j;
+            int r = 255;
+            int g = 0;
+            int b = 0;
 
-    SDL_DestroyWindow(window);
+            putpixel(screen, x, y, SDL_MapRGB(screen->format, r, g, b));
+        }
+    }
+
+    SDL_UpdateWindowSurface(win);
+    SDL_Delay(5000);
+    
+
+    SDL_DestroyWindow(win);
     SDL_Quit();
-
     return 0;
+}
+
+Uint32 getpixel(SDL_Surface* surface, int x, int y)
+{
+    int bpp = surface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to retrieve */
+    Uint8* p = (Uint8*)surface->pixels + y * surface->pitch + x * bpp;
+
+    switch (bpp) {
+    case 1:
+        return *p;
+        break;
+
+    case 2:
+        return *(Uint16*)p;
+        break;
+
+    case 3:
+        if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
+            return p[0] << 16 | p[1] << 8 | p[2];
+        else
+            return p[0] | p[1] << 8 | p[2] << 16;
+        break;
+
+    case 4:
+        return *(Uint32*)p;
+        break;
+
+    default:
+        return 0;       /* shouldn't happen, but avoids warnings */
+    }
+}
+
+void putpixel(SDL_Surface* surface, int x, int y, Uint32 pixel)
+{
+    int bpp = surface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to set */
+    Uint8* p = (Uint8*)surface->pixels + y * surface->pitch + x * bpp;
+
+    switch (bpp) {
+    case 1:
+        *p = pixel;
+        break;
+
+    case 2:
+        *(Uint16*)p = pixel;
+        break;
+
+    case 3:
+        if (SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+            p[0] = (pixel >> 16) & 0xff;
+            p[1] = (pixel >> 8) & 0xff;
+            p[2] = pixel & 0xff;
+        }
+        else {
+            p[0] = pixel & 0xff;
+            p[1] = (pixel >> 8) & 0xff;
+            p[2] = (pixel >> 16) & 0xff;
+        }
+        break;
+
+    case 4:
+        *(Uint32*)p = pixel;
+        break;
+    }
 }
