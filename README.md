01da1d4a6a49fc95c8d168fc9281a36fd87bb831
WanLittle <2621996061@qq.com>
2020-03-13 12:19:10 +0800

    feat: z-buffers z-test z-write

--- README.md
+++ README.md
@@ -31,3 +31,5 @@ https://github.com/Microsoft/vcpkg
   - https://www.willusher.io/sdl2%20tutorials/2013/08/17/lesson-1-hello-world
   - http://sdl.beuc.net/sdl.wiki/Pixel_Access
 - Bresenham直线绘制算法：https://zh.wikipedia.org/wiki/%E5%B8%83%E9%9B%B7%E6%A3%AE%E6%BC%A2%E5%A7%86%E7%9B%B4%E7%B7%9A%E6%BC%94%E7%AE%97%E6%B3%95
+- 扫描线算法：https://zh.wikipedia.org/wiki/%E6%89%AB%E6%8F%8F%E7%BA%BF%E6%B8%B2%E6%9F%93
+
--- miniRenderer/main.cpp
+++ miniRenderer/main.cpp
@@ -9,8 +9,9 @@
 const int width = 800;
 const int height = 800;
 
+Vec3f world2screen(Vec3f v);
 void line(Vec2i v1, Vec2i v2, std::shared_ptr<Window>& win, Uint32 color);
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, std::shared_ptr<Window>& win, Uint32 color);
+void triangle(Vec3f* pts, float* zbuffer, std::shared_ptr<Window>& win, Uint32 color);
 
 int main(int argc, char* argv[])
 {
@@ -20,28 +21,24 @@ int main(int argc, char* argv[])
     Uint32 white = SDL_MapRGB(win->mCanvas->format, 255, 255, 255);
     Uint32 red = SDL_MapRGB(win->mCanvas->format, 255, 0, 0);
 
+    std::cout << std::numeric_limits<double>::max() << std::endl;
+    std::cout << std::numeric_limits<double>::min() << std::endl;
+    std::cout << std::numeric_limits<float>::max() << std::endl;
+    std::cout << std::numeric_limits<float>::min() << std::endl;
+    std::cout << std::numeric_limits<int>::max() << std::endl;
+    std::cout << std::numeric_limits<int>::min() << std::endl;
+
+    float* zbuffer = new float[width * height];
+    for (int i = width * height; i--; zbuffer[i] = -std::numeric_limits<float>::max());
+
     Vec3f light_dir(0, 0, -1);
 
     for (int i = 0; i < model->nfaces(); i++) 
     {
         std::vector<int> face = model->face(i);
-        Vec2i screen_coords[3];
-        Vec3f world_coords[3];
-        for (int j = 0; j < 3; j++) 
-        {
-            Vec3f v = model->vert(face[j]);
-            screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
-            world_coords[j] = v;
-        }
-        Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
-        n.normalize();
-        float intensity = n * light_dir;
-        if (intensity > 0) // 背面图元 intensity<0 被剔除
-        {
-            triangle(screen_coords[0], screen_coords[1], screen_coords[2], win,
-                SDL_MapRGB(win->mCanvas->format, intensity * 255, intensity * 255, intensity * 255));
-        }
-       
+        Vec3f pts[3];
+        for (int i = 0; i < 3; i++) pts[i] = world2screen(model->vert(face[i]));
+        triangle(pts, zbuffer, win, SDL_MapRGB(win->mCanvas->format, rand() % 255, rand() % 255, rand() % 255));
     }
 
     win->update();
@@ -49,6 +46,11 @@ int main(int argc, char* argv[])
     win->destroy();
 
     return 0;
+}
+
+Vec3f world2screen(Vec3f v)
+{
+    return Vec3f(int((v.x + 1.) * width / 2. + .5), int((v.y + 1.) * height / 2. + .5), v.z);
 }
 
 // Bresenham 直线算法
@@ -87,29 +89,37 @@ void line(Vec2i v1, Vec2i v2, std::shared_ptr<Window>& win, Uint32 color)
 }
 
 // Line sweeping 扫描线算法
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, std::shared_ptr<Window>& win, Uint32 color)
+void triangle(Vec3f* pts, float* zbuffer, std::shared_ptr<Window>& win, Uint32 color)
 {
-    if (t0.y == t1.y && t0.y == t2.y) // 忽略退化的三角形
+    if (pts[0].y == pts[1].y && pts[0].y == pts[2].y) // 忽略退化的三角形
         return;
 
-    if (t0.y > t1.y) std::swap(t0, t1);
-    if (t0.y > t2.y) std::swap(t0, t2);
-    if (t1.y > t2.y) std::swap(t1, t2);
+    if (pts[0].y > pts[1].y) std::swap(pts[0], pts[1]);
+    if (pts[0].y > pts[2].y) std::swap(pts[0], pts[2]);
+    if (pts[1].y > pts[2].y) std::swap(pts[1], pts[2]);
 
-    int total_height = t2.y - t0.y;
+    int total_height = pts[2].y - pts[0].y;
     for (int i = 0; i < total_height; i++) 
     {
-        bool second_half = i > t1.y - t0.y || t1.y == t0.y;
-        int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+        bool second_half = i > pts[1].y - pts[0].y || pts[1].y == pts[0].y;
+        int segment_height = second_half ? pts[2].y - pts[1].y : pts[1].y - pts[0].y;
         float alpha = (float)i / total_height;
-        float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height;
-        Vec2i A = t0 + (t2 - t0) * alpha;
-        Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+        float beta = (float)(i - (second_half ? pts[1].y - pts[0].y : 0)) / segment_height;
+        Vec3f A = pts[0] + (pts[2] - pts[0]) * alpha;
+        Vec3f B = second_half ? pts[1] + (pts[2] - pts[1]) * beta : pts[0] + (pts[1] - pts[0]) * beta;
         if (A.x > B.x) 
             std::swap(A, B);
-        for (int j = A.x; j <= B.x; j++) 
+        int l = (int) A.x, r = (int) B.x;
+        for (int j = l; j <= r; j++)
         {
-            win->setPixelColor(j, t0.y + i, color);
+            float t = (float)(j - l) / (r - l);
+            float z = A.z * (1 - t) + B.z * t;
+            int wPos = j, yPos = pts[0].y + i;
+            if (zbuffer[wPos + yPos * width] < z)
+            {
+                zbuffer[wPos + yPos * width] = z;
+                win->setPixelColor(wPos, yPos, color);
+            }
         }
     }
 }
