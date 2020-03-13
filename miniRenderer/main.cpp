ccb1701a647661b7c22b5d210d1eca082d7afd07
WanLittle <2621996061@qq.com>
2020-03-13 09:27:02 +0800

    feat: 模型文件支持 & fix: 画线算法BUG

--- miniRenderer/main.cpp
+++ miniRenderer/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <algorithm>
+
+#include "model.h"
 #include "SDL.h"
+
+const int width = 800;
+const int height = 800;
 
 void line(int x0, int y0, int x1, int y1, SDL_Surface* screen, Uint32 color);
 Uint32 getpixel(SDL_Surface* surface, int x, int y); // 获取颜色
@@ -15,7 +20,7 @@ int main(int argc, char* argv[])
     }
 
     // 初始化窗口
-    SDL_Window* win = SDL_CreateWindow("mini renderer", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window* win = SDL_CreateWindow("mini renderer", 100, 100, width, height, SDL_WINDOW_SHOWN);
     
     if (win == nullptr) {
         std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
@@ -28,9 +33,22 @@ int main(int argc, char* argv[])
     Uint32 white = SDL_MapRGB(screen->format, 255, 255, 255);
     Uint32 red = SDL_MapRGB(screen->format, 255, 0, 0);
 
-    line(13, 20, 80, 40, screen, white);
-    line(20, 13, 40, 80, screen, red);
-    line(80, 40, 13, 20, screen, red);
+    Model* model = new Model("../assets/african_head.obj");
+
+    for (int i = 0; i < model->nfaces(); i++) 
+    {
+        std::vector<int> face = model->face(i);
+        for (int j = 0; j < 3; j++) 
+        {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j + 1) % 3]);
+            int x0 = (v0.x + 1.0) * width / 2.0;
+            int y0 = (v0.y + 1.0) * height / 2.0;
+            int x1 = (v1.x + 1.0) * width / 2.0;
+            int y1 = (v1.y + 1.0) * height / 2.0;
+            line(x0, y0, x1, y1, screen, white);
+        }
+    }
 
     SDL_UpdateWindowSurface(win);
     SDL_Delay(10000);
@@ -55,12 +73,12 @@ void line(int x0, int y0, int x1, int y1, SDL_Surface* screen, Uint32 color)
         std::swap(y0, y1);
     }
     int deltaX = x1 - x0;
-    int deltaY = y1 - y0;
+    int deltaY = std::abs(y1 - y0);
     int error = deltaX / 2;
     int yStep;
     int y = y0;
     yStep = y0 < y1 ? 1 : -1;
-    for (int x = x0; x <= x1; x++) 
+    for (int x = x0; x <= x1; ++x) 
     {
         if(steep) 
             putpixel(screen, y, x, color);
@@ -72,7 +90,6 @@ void line(int x0, int y0, int x1, int y1, SDL_Surface* screen, Uint32 color)
             y = y + yStep;
             error = error + deltaX;
         }
-        putpixel(screen, x, y, color);
     }
 }
 
@@ -112,6 +129,8 @@ void putpixel(SDL_Surface* surface, int x, int y, Uint32 pixel, bool flipY)
     if (flipY) y = surface->h - y;
     int bpp = surface->format->BytesPerPixel;
     /* Here p is the address to the pixel we want to set */
+    x = std::max(0, std::min(surface->w - 1, x));
+    y = std::max(0, std::min(surface->h - 1, y));
     Uint8* p = (Uint8*)surface->pixels + y * surface->pitch + x * bpp;
     
     switch (bpp) 
